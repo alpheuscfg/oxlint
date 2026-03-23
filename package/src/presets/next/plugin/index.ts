0fb9c108b02249a2314a25aa2153156135d7892d
Alpheus <26622568+alpheusmtx@users.noreply.github.com>
2026-03-23 20:31:40 +0800

    refactor: make preset run individually

--- package/src/presets/next/plugin/index.ts
+++ package/src/presets/next/plugin/index.ts
@@ -1,9 +1,6 @@
 import type { LintPlugins } from "#/@types/oxlint";
 
-import { PLUGIN_REACT } from "#/presets/react/plugin";
-
 const PLUGIN_NEXT = [
-    ...PLUGIN_REACT,
     "nextjs",
 ] as const satisfies LintPlugins;
 
