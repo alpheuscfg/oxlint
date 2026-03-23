c65c8a9461af5a4d41eba8368d3a337e32c08490
Alpheus <26622568+alpheusmtx@users.noreply.github.com>
2026-03-23 14:52:54 +0800

    refactor: update next preset

--- package/src/presets/next/preset/index.ts
+++ package/src/presets/next/preset/index.ts
@@ -4,17 +4,17 @@ import type { Preset, PresetResult } from "#/@types/preset";
 
 import { toMerged } from "es-toolkit";
 
-import { PLUGIN_REACT } from "#/presets/react/plugin";
-import { RULES_REACT } from "#/presets/react/rules/react";
+import { PLUGIN_NEXT } from "#/presets/next/plugin";
+import { RULES_NEXTJS } from "#/presets/next/rules/nextjs";
 
 const nextPreset = (): Preset => {
     return ({ config: internalConfig }): PresetResult => {
         const config: OxlintConfig = toMerged(internalConfig, {
             plugins: [
-                ...PLUGIN_REACT,
+                ...PLUGIN_NEXT,
             ],
             rules: {
-                ...RULES_REACT,
+                ...RULES_NEXTJS,
             },
         } satisfies OxlintConfig);
 
